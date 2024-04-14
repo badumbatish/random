76a1d5f213ee5adbf3d1224af731bdd69b9e9915
Jasmine T <tanghocle456@gmail.com>
2024-04-13 22:47:22 -0700

    Add minimum support for cli with echo

---
+++ src/cli_arg.rs
@@ -0,0 +1,29 @@
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Cli {
+    /// Commands to operate on
+    #[command(subcommand)]
+    pub command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// does testing things
+    Echo(Echo),
+}
+
+#[derive(Args)]
+pub struct Echo {
+    message: Vec<String>,
+}
+impl Echo {
+    pub fn echo_back(&self) {
+        for i in &self.message {
+            print!("{} ", i);
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
+mod cli_arg;
+use cli_arg::{Cli as cli, Echo};
 mod util;
-
+use clap::Parser;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = cli::parse();
     // input a string that is our directory
-    let s = util::get_input_dir();
+    let s = ".";
     let v = util::get_test_files(&s);
 
     for i in v {
         println!("{}", i);
+    }
+    match cli.command {
+        Some(cli_arg::Commands::Echo(echo)) => {
+            echo.echo_back();
+        }
+        None => {
+            println!("No command given");
+        }
     }
     Ok(())
 }
